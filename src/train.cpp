e3d2fd0d2de0231b093192ccf9519e9fdc7e72cc
BushSasha <sashabush2006@gmail.com>
2025-05-16 13:23:09 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,38 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+int Train::getLength() {
+    countOp = 0;
+    if (!first) return 0;
+    if (!first->light) {
+        first->light = true;
+    }
+    Car* start = first;
+    int max_steps = 1;
+    while (true) {
+        int steps = 0;
+        Car* current = start;
+        bool found = false;
+        while (steps < max_steps) {
+            current = current->next;
+            steps++;
+            countOp++;
+            if (current->light) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            if (current == start) {
+                return steps;
+            }
+            current->light = false;
+            for (int i = 0; i < steps; ++i) {
+                current = current->prev;
+                countOp++;
+            }
+        } else {
+            max_steps++;
+        }
+    }
+}
